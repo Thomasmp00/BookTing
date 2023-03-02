718ebf78a9eb11a8c4e9dd539605067aac21aaad
KasperTell <kaspertell@gmail.com>
2023-03-02 10:15:27 +0100

    REEE

--- src/dk/javahandson/BLL/BookManager.java
+++ src/dk/javahandson/BLL/BookManager.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public class BookManager {
 
-    public List<Book> getAllBooks() throws Exception {
+    /**public List<Book> getAllBooks() throws Exception {
         return bookDAO_DB.getAllBooks;
     }
 
     public Book createBook(String title, String forfatter, String forlag, String isbn, int rating, Date laestDato, Date udlaantDate){
         return bookDAO_DB.createBook(title, forfatter, forlag, isbn, rating, laestDato, udlaantDate);
-    }
+    }*/
 
 }
